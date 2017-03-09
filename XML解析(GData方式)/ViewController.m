10b4967656ed4d2e2ed510dfa0c8c48210d2f727
LockLight <Locklightz@gmail.com>
2017-03-09 20:13:28 +0800

    XML解析(GData方式)

--- XML解析(GData方式)/ViewController.m
+++ XML解析(GData方式)/ViewController.m
@@ -7,16 +7,52 @@
 //
 
 #import "ViewController.h"
+#import "Video.h"
+#import "GDataXMLNode.h"
 
 @interface ViewController ()
 
 @end
 
-@implementation ViewController
+@implementation ViewController{
+    NSMutableArray<Video *> *_modelList;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _modelList = [NSMutableArray array];
+    
+    [self loadData];
+}
+
+- (void)loadData{
+    NSURL *url = [NSURL URLWithString:@"http://127.0.0.1/videos02.xml"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    [[[NSURLSession sharedSession] dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        if (error != nil){
+            NSLog(@"error =%@",error);
+            return;
+        }
+        
+        //GData创建XML文档树
+        GDataXMLDocument *doc = [[GDataXMLDocument alloc]initWithData:data error:NULL];
+        
+        //遍历主干子节点
+        for (GDataXMLElement *videoEle in doc.rootElement.children) {
+            Video *model = [Video new];
+            //遍历子节点的值
+            for (GDataXMLElement *element in videoEle.children) {
+                [model setValue:element.stringValue forKey:element.name];
+            }
+            //遍历子节点的属性
+            for (GDataXMLNode * node in videoEle.attributes ) {
+                [model setValue:node.stringValue forKey:node.name];
+            }
+            [_modelList addObject:model];
+        }
+        NSLog(@"%@",_modelList);
+    }]resume];
 }
 
 
